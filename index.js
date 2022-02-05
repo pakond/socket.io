e3bc41077ab3b1b106846b961d80328f832331fe
Pau Albert Gandul <palbert@benchmarking30.com>
2022-02-06 00:11:50 +0100

    first socket io

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const frontVersion = '0.0.2';
+const fs = require( 'fs' );
+const express = require("express");
+const app = express();
+const https = require("https");
+const server = https.createServer({ 
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('fullchain.pem') 
+},app).listen(3001);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+    cors: {
+      origin: "https://newcmh-dev.benchmarking30.com",
+      credentials: true
+    }
+});
+
+io.on("connection", (socket) => {
+  console.log('connection established');
+  socket.on("checkVersion", (id, version) => {
+    if (version !== frontVersion) {
+      socket.emit("resolveVersion");
+    }
+  });
+});
