4eda2805d5d617b9d550aa04738682db2043f4b4
Pau Albert Gandul <palbert@benchmarking30.com>
2022-02-13 09:49:44 +0100

    last build

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const frontVersion = '0.0.2';
-const fs = require( 'fs' );
+const frontVersion = "0.0.4";
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const https = require("https");
-const server = https.createServer({ 
-  key: fs.readFileSync('privkey.pem'),
-  cert: fs.readFileSync('fullchain.pem') 
-},app).listen(3001);
+const server = https
+  .createServer(
+    {
+      key: fs.readFileSync("privkey.pem"),
+      cert: fs.readFileSync("fullchain.pem"),
+    },
+    app
+  )
+  .listen(3001);
 const { Server } = require("socket.io");
 
 const io = new Server(server, {
-    cors: {
-      origin: "https://newcmh-dev.benchmarking30.com",
-      credentials: true
-    }
+  cors: {
+    origin: "https://newcmh-dev.benchmarking30.com",
+    credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log('connection established');
+  console.log("connection established");
   socket.on("checkVersion", (id, version) => {
     if (version !== frontVersion) {
       socket.emit("resolveVersion");
